6ff5ae2ddf5fe6c860bb1f7e19eed0540493d928
Zafer AYAN <zaferayan@teknasyon.com>
2022-01-13 15:46:29 +0300

    Fix miniapp

---
+++ ios/ErnRunner/RunnerConfig.swift
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2021 WalmartLabs
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+//
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// GENERATED RUNNER CONFIGURATION
+// DO NOT EDIT MANUALLY
+// CHANGES TO THIS FILE WILL BE OVERWRITTEN BY NEXT RUN OF ern run-ios COMMAND
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+import Foundation
+
+struct RunnerConfig {
+    static let mainMiniAppName = "MoviesReloaded"
+    static let rnDevSupportEnabled = true
+    static let packagerHost = "192.168.50.112"
+    static let packagerPort = "8081"
+}
